37f98939e6498a78bae2541b2015667833b9bb2a
Ely Asaf <elyas@edu.hac.ac.il>
2023-05-30 14:58:38 +0300

    Nadav oni-chann onegai shimasu!

--- movie-app/src/components/searchPage/FormInputRow.js
+++ movie-app/src/components/searchPage/FormInputRow.js
@@ -1,20 +1,33 @@
-import {Row,Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import React from 'react';
 
-const FormInputRow = ({labelForText,labelTxt, onChangeHandler, valueState}) =>{
-    return(<>
-    <Row>
-        <Col>
-            <label htmlFor={labelForText} className={'form-label'}>{labelTxt}</label>
-            <input name={labelForText}
-                type="text"
-                id={labelForText}
-                value={valueState}
-                onChange={onChangeHandler}
-                className={"form-control"}/>
-        </Col>
-    </Row>
-    </>)
+/**
+ * A component representing a form input row with a label and an input field.
+ * @param {Object} props - The component props.
+ * @param {string} props.labelForText - The value for the `for` attribute of the label.
+ * @param {string} props.labelTxt - The label text.
+ * @param {Function} props.onChangeHandler - The event handler for the input field's `onChange` event.
+ * @param {string} props.valueState - The value of the input field.
+ * @returns {JSX.Element} The rendered FormInputRow.
+ */
+const FormInputRow = ({ labelForText, labelTxt, onChangeHandler, valueState }) => {
+    return (
+        <>
+            <Row>
+                <Col>
+                    <label htmlFor={labelForText} className={'form-label'}>{labelTxt}</label>
+                    <input
+                        name={labelForText}
+                        type="text"
+                        id={labelForText}
+                        value={valueState}
+                        onChange={onChangeHandler}
+                        className={"form-control"}
+                    />
+                </Col>
+            </Row>
+        </>
+    );
 };
 
 export default FormInputRow;
--- movie-app/src/components/searchPage/SearchList.js
+++ movie-app/src/components/searchPage/SearchList.js
@@ -1,46 +1,76 @@
 import React from "react";
-import {Button, Row} from "react-bootstrap";
+import { Button, Row } from "react-bootstrap";
 
-import {ACTION_TYPES} from "./ACTION_TYPES";
+import { ACTION_TYPES } from "./ACTION_TYPES";
 
-const SearchList = ({state,dispatch}) =>{
-    const clearHistory = () =>{
-        dispatch({type:ACTION_TYPES.CLEAR_HIST, payload:{}});
+/**
+ * A component that displays the search history and provides actions for managing it.
+ * @param {Object} props - The component props.
+ * @param {Object} props.state - The state object containing the search history.
+ * @param {Function} props.dispatch - The dispatch function used to trigger state updates.
+ * @returns {JSX.Element} The rendered SearchList component.
+ */
+const SearchList = ({ state, dispatch }) => {
+    /**
+     * Clears the search history.
+     */
+    const clearHistory = () => {
+        dispatch({ type: ACTION_TYPES.CLEAR_HIST, payload: {} });
+    };
 
-    }
+    /**
+     * Deletes a specific search entry from the search history.
+     * @param {number} index - The index of the search entry to delete.
+     */
     const deleteSearch = (index) => {
-        dispatch({type:ACTION_TYPES.DELETE_SEARCH, payload:{index: index}});
+        dispatch({ type: ACTION_TYPES.DELETE_SEARCH, payload: { index: index } });
     };
 
+    /**
+     * Performs a search using the provided search entry.
+     * @param {Object} search - The search entry to use for the search.
+     */
     const handleSearchAgain = (search) => {
-        dispatch({type:ACTION_TYPES.SEARCH_AGAIN, payload:{searchEntry: search}});
+        dispatch({ type: ACTION_TYPES.SEARCH_AGAIN, payload: { searchEntry: search } });
     };
-    return(<div>
-        <h3>Search History</h3>
-        <hr/>
-        {
-            (state.searchHistory.length && state.searchHistory.length >0) ?
-                <Button className={'btn btn-danger'} onClick={clearHistory}>Clear Search History</Button>
-                :
-                <b> Search history is currently empty</b>
-        }
 
-        {state.searchHistory.map((search, index) => (
-            <div key={index}>
-                <p> <strong>Search String:</strong>  {search.searchString ? search.searchString : 'Empty parameter'}</p>
-                <br/>
-                <p> <strong>Selected Genres:</strong> {search.selectedGenres ? search.selectedGenres.join('|') : 'Empty parameter'}</p>
-                <br/>
-                <p> <strong> Release Year:</strong> {search.releaseYear ? search.releaseYear : 'Empty parameter'}</p>
-                <Row>
+    return (
+        <div>
+            <h3>Search History</h3>
+            <hr />
+            {state.searchHistory.length && state.searchHistory.length > 0 ? (
+                <Button className={'btn btn-danger'} onClick={clearHistory}>
+                    Clear Search History
+                </Button>
+            ) : (
+                <b> Search history is currently empty</b>
+            )}
+
+            {state.searchHistory.map((search, index) => (
+                <div key={index}>
                     <p>
-                        <Button onClick={() => deleteSearch(index)}>Delete Search</Button>
-                        <Button onClick={() => handleSearchAgain(search)}>Search Again</Button>
+                        <strong>Search String:</strong> {search.searchString ? search.searchString : 'Empty parameter'}
                     </p>
-                </Row>
-                <hr />
-            </div>
-        ))}
-    </div>)
+                    <br />
+                    <p>
+                        <strong>Selected Genres:</strong>{' '}
+                        {search.selectedGenres ? search.selectedGenres.join('|') : 'Empty parameter'}
+                    </p>
+                    <br />
+                    <p>
+                        <strong>Release Year:</strong> {search.releaseYear ? search.releaseYear : 'Empty parameter'}
+                    </p>
+                    <Row>
+                        <p>
+                            <Button onClick={() => deleteSearch(index)}>Delete Search</Button>
+                            <Button onClick={() => handleSearchAgain(search)}>Search Again</Button>
+                        </p>
+                    </Row>
+                    <hr />
+                </div>
+            ))}
+        </div>
+    );
 };
+
 export default SearchList;
